a7bd1f12ea6980663411ed38758c137f0c9a1a84
Gustavo Siqueira <92535703+SiqueiraGu@users.noreply.github.com>
2021-10-14 12:48:24 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A, B, X;
+
+            A = int.Parse(Console.ReadLine());
+            B = int.Parse(Console.ReadLine());
+
+            X = A + B;
+
+            Console.WriteLine("X = " + X);
+        }
+    }
+}
